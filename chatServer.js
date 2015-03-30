725bfa47b02e17489df356c4dc2ee99513816c5d
Setzer <molly.setzer@gmail.com>
2015-03-30 07:53:09 -0400

    edited client and server

--- chatServer.js
+++ chatServer.js
@@ -1,44 +1,40 @@
 var net = require('net');
 var port = 3000;
 var history = [];
-var participant = [];
+var client = [];
+
 
 var server = net.createServer(function(socket) {
   console.log('client connected');
   socket.write('Welcome to chat!' + history);
 
-  if (data.length > 0) {
-        var history === push.data.toString().trim();
+  	clients.push(socket);
+
+   socket.on('end', function() {
+    console.log('client disconnected');
+ 	if (data.length > 0) {
+        JSON.stringify(var history === push.data.toString().trim());
+    } else{
+    	console.log('err');
     }
+	});
+
+
+   var history = JSON.stringify(history[i]);
+       for (var i=0; i < clients.length; i++) {
+			console.log(history[i]);
+             };
 
   socket.on('data', function(data) {
     console.log(data.toString().trim());
   });
 
 
-  	/* after 
-  } else { // log and broadcast the message
-                console.log((new Date()) + ' Received Message from '
-                            + userName + ': ' + message.utf8Data);
-                
-                // we want to keep history of all sent messages
-                var obj = {
-                    time: (new Date()).getTime(),
-                    text: htmlEntities(message.utf8Data),
-                    author: userName,
-                    color: userColor
-                };
-                */
-
-
-
-  socket.on('end', function() {
-    console.log('client disconnected');
-  });
-  if (history.length > 0) {
-        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
-    }
-});
+ function broadcast(message, sender) {
+    clients.forEach(function (client) {
+      if (client === sender) return;
+      client.write(message);
+    });
 
 server.listen(port, function() { 
   console.log('listening on port ' + port );
